19af2c36887e5306d1e218b808a4548e0d6854ae
EbenGitHub <abenezergoo@gmail.com>
2022-12-19 18:40:07 +0300

    eslint corr

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-   const task = true;
-   const task2 = false;
+    const task = true;
+    console.log(task);
+    const task2 = false;
+    console.log(task2);
   }
 
   return [task, task2];

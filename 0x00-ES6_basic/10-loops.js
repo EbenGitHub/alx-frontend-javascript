28edba686023578acfc9a7f1b5db97229eb88a6f
EbenGitHub <abenezergoo@gmail.com>
2022-12-21 10:15:20 +0300

    corr es lint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // for loop 
-	for (const value of array) {
+  // for loop
+  for (const value of array) {
     // arr reassn
-		array[array.indexOf(value)] = appendString + value;
+    array[array.indexOf(value)] = appendString + value;
   }
 
   return array;

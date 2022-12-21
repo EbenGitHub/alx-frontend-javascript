196dd3c08ddf62447ccb0740dac1f74abb666873
EbenGitHub <abenezergoo@gmail.com>
2022-12-21 11:07:53 +0300

    obj meth

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,12 +3,8 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars() {
-      return `$${budget.income}`;
-    },
-    getIncomeInEuros() {
-      return `${budget.income} euros`;
-    },
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
   };
 
   return fullBudget;

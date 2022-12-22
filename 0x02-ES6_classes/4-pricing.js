b9b5b565d66162cfff0336bebd0db09e19fecc8e
EbenGitHub <abenezergoo@gmail.com>
2022-12-23 00:09:12 +0300

    Currency obj used and eslint corr

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,22 +1,33 @@
 import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
+    // Create objs
     this._amount = amount;
     this._currency = currency;
   }
 
-  // Setter
+  // Methods
+
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
+  // Setters
   set amount(newAmount) {
     this._amount = newAmount;
   }
 
   set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) throw TypeError('current must be an instance of Currency');
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
     this._currency = newCurrency;
   }
 
-  // Getter
+  // Getters
   get amount() {
     return this._amount;
   }
@@ -24,15 +35,4 @@ class Pricing {
   get currency() {
     return this._currency;
   }
-
-  // Methods
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
-
-  static convertPrice(amount, conversionRate) {
-    return `${amount * conversionRate}`;
-  }
 }
-
-export default Pricing;

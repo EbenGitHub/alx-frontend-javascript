daafa348e4a7001598858e5180a7e0a50dc4531f
EbenGitHub <abenezergoo@gmail.com>
2022-12-22 22:43:22 +0300

    setter and getter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,30 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') throw TypeError('Name must be a string');
+    if (typeof (length) !== 'number') throw TypeError('Length must be a number');
+    if (typeof (students) !== 'object') throw TypeError('Students must be an arrow');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof (name) !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof (length) !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+}
+
+export default HolbertonCourse;

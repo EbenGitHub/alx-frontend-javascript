f0101783b227a479266845ec4af34ff02e230bbe
EbenGitHub <abenezergoo@gmail.com>
2022-12-22 23:21:49 +0300

    sett gett meth and eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,33 @@
 export default class Currency {
   constructor(code, name) {
-    if (Object.getPrototypeOf(name) !== String.prototype) throw TypeError("Name must be a string");
-    if (Object.getPrototypeOf(code) !== String.prototype) throw TypeError("Code must be a string");
+    if (Object.getPrototypeOf(name) !== String.prototype) throw TypeError('Name must be a string');
+    if (Object.getPrototypeOf(code) !== String.prototype) throw TypeError('Code must be a string');
     this._code = code;
     this._name = name;
   }
 
-
   // Setters
-  set code(newCode){
-    if (Object.getPrototypeOf(newCode) !== String.prototype) throw TypeError("Code must be a string");
+  set code(newCode) {
+    if (Object.getPrototypeOf(newCode) !== String.prototype) throw TypeError('Code must be a string');
     this._code = newCode;
   }
 
-  set code(newName){
-    if (Object.getPrototypeOf(newName) !== String.prototype) throw TypeError("Code must be a string");
+  set name(newName) {
+    if (Object.getPrototypeOf(newName) !== String.prototype) throw TypeError('Code must be a string');
     this._name = newName;
   }
 
   // Getters
   get code() {
-  return this._code;
+    return this._code;
   }
 
   get name() {
-  return this._name;
+    return this._name;
   }
 
   // Methods
   displayFullCurrency() {
-  return `${this._name} (${this._code})`}
+    return `${this._name} (${this._code})`;
+  }
 }

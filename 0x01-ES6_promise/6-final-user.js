732c2a8f27224e3d28016bfaa9bf94954b091ed5
EbenGitHub <abenezergoo@gmail.com>
2022-12-23 16:20:36 +0300

    mor vr prom

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const s = signUpUser(firstName, lastName);
+  const u = uploadPhoto(fileName);
 
-export default handleProfileSignup;
+  return Promise.allSettled([s, u]).then((vals) => {
+    const resArr = [];
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        resArr.push({ status: val.status, value: val.value });
+      } else {
+        resArr.push({
+          status: val.status,
+          value: `Error: ${val.reason.message}`,
+        });
+      }
+    });
+    return resArr;
+  });
+}

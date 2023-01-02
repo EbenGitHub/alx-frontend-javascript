3c805490092b6530360acfedfb2340b8362dad8a
EbenGitHub <abenezergoo@gmail.com>
2023-01-03 00:05:38 +0300

    valid add

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,12 @@
 export default function cleanSet(set, startString) {
   const startSet = [];
-  if (startString === '') return '';
+  if (
+    startString === ''
+    || set.constructor !== Set
+    || startString.constructor !== String
+  ) {
+    return '';
+  }
 
   set.forEach((s) => {
     if (s.startsWith(startString)) startSet.push(s.slice(startString.length));

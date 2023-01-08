1e26a1541e73fac806d474ca2fecb7eb6c1d796a
EbenGitHub <abenezergoo@gmail.com>
2023-01-08 17:17:50 +0300

    eslint corr

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString == undefined) return ''
+  if (startString === undefined) return '';
   const startSet = [];
   if (
     set.constructor === Set
@@ -7,8 +7,9 @@ export default function cleanSet(set, startString) {
     && startString
   ) {
     set.forEach((s) => {
-      if ((typeof s === 'string') && (s.startsWith(startString)))
-          startSet.push(s.slice(startString.length));
+      if ((typeof s === 'string') && (s.startsWith(startString))) {
+        startSet.push(s.slice(startString.length));
+      }
     });
   }
 
